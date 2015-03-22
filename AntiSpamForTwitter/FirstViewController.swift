c39fb63b05a1e34c21984dcb35c8310aa2c51540
Kensuke Hoshikawa <kensuke1751@gmail.com>
2015-03-22 17:10:10 +0900

    Add PrasedTwitterAppHTML File.
    Move parse twitter app from FirstViewController to ParsedTwitterAppHTML.

--- AntiSpamForTwitter/FirstViewController.swift
+++ AntiSpamForTwitter/FirstViewController.swift
@@ -8,30 +8,151 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UIWebViewDelegate {
+    
+    let myItems: NSArray = ["TEST1", "TEST2", "TEST3"]
+    let PC_CHROME_UA = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36"
+    var defalutUA:String!
+    var application:[[String:Any]] = [[:]]
+    let myWebView : UIWebView = UIWebView()
+    var myTable : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        var web = TwitterWebView(frame:self.view.bounds)
-        web.createWebView()
-        self.view.addSubview(web)
-
-        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
-        let navBarHeight: CGFloat? = self.navigationController?.navigationBar.frame.size.height
-        let displayWidth: CGFloat = self.view.frame.width
-        let displayHeight: CGFloat = self.view.frame.height / 2
-        let frame = CGRect(x: 0, y: barHeight + navBarHeight!, width: displayWidth, height: displayHeight - barHeight - navBarHeight!)
-
-        var table = TwitterAppTableView(frame:frame,style:UITableViewStyle.Plain)
-        table.createTableView()
-        self.view.addSubview(table)
+        
+        defalutUA = getDefaultUA()
+        setWebViewParams()
+        self.view.addSubview(myWebView)
+        
+        setTableViewParams()
+        self.view.addSubview(myTable)
     }
-
+    
+    func setTableViewParams(){
+        myTable.dataSource = self
+        myTable.delegate = self
+        let frame = getWindowSize()
+        myTable = UITableView(frame: frame)
+        
+        // Cell名の登録をおこなう.
+        myTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+    }
+    
+    func setWebViewParams(){
+        myWebView.delegate = self
+        myWebView.frame = self.view.bounds
+        let url: NSURL = NSURL(string: TwitterUrls.LOGIN.rawValue)!
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        myWebView.loadRequest(request)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
+    // 初期状態のUAを保存しておく
+    func getDefaultUA() -> String{
+        let webView:UIWebView = UIWebView()
+        webView.frame = CGRectZero
+        let userAgent:String! = webView.stringByEvaluatingJavaScriptFromString("navigator.userAgent")
+        return userAgent
+    }
+    
+    func getWindowSize() -> CGRect{
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        let navBarHeight: CGFloat? = self.navigationController?.navigationBar.frame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        let frame = CGRect(x: 0, y: barHeight + navBarHeight! + 300, width: displayWidth, height: displayHeight - barHeight - navBarHeight!)
+        
+        return frame
+    }
+    
+    // UA書き換え
+    func changeUserAgent(){
+        let userAgentStr = PC_CHROME_UA
+        let dic:NSDictionary = ["UserAgent":userAgentStr]
+        NSUserDefaults.standardUserDefaults().registerDefaults(dic)
+    }
+    
+    // Pageがすべて読み込み終わった時呼ばれる
+    func webViewDidFinishLoad(webView: UIWebView!) {
+        println("webViewDidFinishLoad")
+        let js = "document.body.innerHTML"
+        let body = webView.stringByEvaluatingJavaScriptFromString(js)
+        let parsedTwitterAppHTML = ParsedTwitterAppHTML(twitterAppHtml: body!)
+        parsedTwitterAppHTML.parseHtml()
+        if let currentUrl = webView.request?.URL.absoluteString {
+            println(currentUrl)
+            if currentUrl == TwitterUrls.INDEX.rawValue {
+                println("login true")
+            }else{
+                println("login false")
+            }
+        }
+        //                logoutTwitter()
+    }
+    
+    // ログアウト
+    func logoutTwitter(){
+        let logoutId = "document.getElementById('signout-button').click();"
+        //        twitterWebView.stringByEvaluatingJavaScriptFromString(logoutId)
+        var storage : NSHTTPCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        for cookie in storage.cookies  as [NSHTTPCookie]{
+            storage.deleteCookie(cookie)
+        }
+        
+        NSUserDefaults.standardUserDefaults()
+        //        var cookie: NSHTTPCookie = NSHTTPCookie()
+        var cookieJar: NSHTTPCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        for cookie in cookieJar.cookies as [NSHTTPCookie]{
+            NSLog("%@", cookie)
+        }
+    }
+    
+    
+    // Pageがloadされ始めた時、呼ばれる
+    func webViewDidStartLoad(webView: UIWebView!) {
+        println("webViewDidStartLoad")
+        println(webView.request?.URL)
+    }
+    
+    /*
+    Cellが選択された際に呼び出される.
+    */
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Num: \(indexPath.row)")
+        println("Value: \(myItems[indexPath.row])")
+    }
+    
+    /*
+    Cellの総数を返す.
+    */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+    /*
+    Cellに値を設定する.
+    */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // Cellの.を取得する.
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as UITableViewCell
+        
+        // Cellに値を設定する.
+        cell.textLabel!.text = "\(myItems[indexPath.row])"
+        
+        return cell
+    }
+    
 }
 
+private enum TwitterUrls: String {
+    case LOGIN = "https://mobile.twitter.com/login/"
+    case INDEX = "https://mobile.twitter.com/"
+    case LOGIN_PC = "https://twitter.com/login/"
+    case INDEX_PC = "https://twitter.com/"
+    case APPLICATIONS = "https://twitter.com/settings/applications/"
+}
 
---
+++ AntiSpamForTwitter/ParsedTwitterAppHTML.swift
@@ -0,0 +1,76 @@
+//
+//  ParsedTwitterAppHTML.swift
+//  AntiSpamForTwitter
+//
+//  Created by Kensuke Hoshikawa on 2015/03/21.
+//  Copyright (c) 2015年 star__hoshi. All rights reserved.
+//
+
+import Foundation
+
+class ParsedTwitterAppHTML{
+    
+    let html:String!
+    
+    init(twitterAppHtml:String){
+        html = twitterAppHtml
+    }
+    
+    
+    /**
+    TwitterのHTMLをParseする
+    
+    :param: html <#html description#>
+    */
+    func parseHtml() -> [[String:Any]]{
+        var err : NSError?
+        let option = CInt(HTML_PARSE_NOERROR.value | HTML_PARSE_RECOVER.value)
+        let parser     = HTMLParser(html: html, encoding: NSUTF8StringEncoding, option: option, error: &err)
+        if err != nil {
+            println(err)
+            exit(1)
+        }
+        
+        let bodyNode   = parser.body
+        var applicationDetail = [String:Any]()
+        var application:[[String:Any]] = [[:]]
+        
+        if let path = bodyNode?.xpath("//div[@class='stream-item oauth-application ']") {
+            for node in path {
+                applicationDetail = getApplicationData(node)
+                application.append(applicationDetail)
+            }
+        }
+        println(application)
+        return application
+    }
+
+
+    /**
+    Twitter の Applicationデータ を取得する
+
+    :param: node <#node description#>
+
+    :returns: <#return value description#>
+    */
+    private func getApplicationData(node: HTMLNode) -> [String:Any]{
+        var applicationDetail = [String:Any]()
+        var metadata: String
+        
+        applicationDetail["name"] = node.findChildTag("strong")?.contents as String!
+        applicationDetail["button_id"] = node.findChildTag("button")?.getAttributeNamed("id")
+        applicationDetail["image"] = node.findChildTag("img")?.getAttributeNamed("src")
+        applicationDetail["creater"] = node.findChildTagAttr("a", attrName: "class", attrValue: "oauth-organization")?.contents as String!
+        applicationDetail["creater_href"] = node.findChildTagAttr("a", attrName: "class", attrValue: "oauth-organization")?.getAttributeNamed("href")
+        applicationDetail["description"] = node.findChildTagAttr("p", attrName: "class", attrValue: "description")?.contents as String!
+        applicationDetail["allow_date"] = node.findChildTagsAttr("small", attrName: "class", attrValue: "metadata")[1].contents as String!
+        
+        metadata = node.findChildTagsAttr("small", attrName: "class", attrValue: "metadata")[0].contents as String!
+        applicationDetail["read"] = metadata.rangeOfString("読み")?.startIndex
+        applicationDetail["write"] = metadata.rangeOfString("書き")?.startIndex
+        applicationDetail["dm"] = metadata.rangeOfString("ダイレクトメッセージ")?.startIndex
+        
+        return applicationDetail
+    }
+    
+}
